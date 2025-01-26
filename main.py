1068d111bf2fa21097fae47e1856acd5ffac4909
Alexandre Rueda Payen <alexruedapayen@gmail.com>
2025-01-26 18:44:52 +0100

    Initializer working when structure already exists

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from Google_API.Google_Drive_Handler import Google_Drive_Handler
-from Google_API.Google_Drive_Initizalizer import Google_Drive_Initizalizer
+from Google_API.Google_Drive_Initializer import Google_Drive_Initializer
 from utils import DotDict
 
 from data.country_markets import country_markets
 
 def main():
 
+    
 
     # Authenticate and initialize Google Drive Handler
     handler = Google_Drive_Handler(
-        root_folder_id=DotDict(path="secrets.Google_Drive.folder_ids").root,
+        root_folder_id=DotDict(path="secrets/Google_Drive/folder_ids.json").root,
         log_dir="./real_logfiles",
         token_path="./secrets/Google_Drive/token.json",
         credentials_path="./secrets/Google_Drive/creds.json"
@@ -17,10 +18,13 @@ def main():
 
     
 
+    # Initialize GoogleDriveInitializer
+    initializer = Google_Drive_Initializer(handler.service)
+
     # Only initialize the folder structure if not already done
-    if not check_if_initialized():
-        initialize_google_drive_folders(handler.service, country_markets, root_folder_id=DotDict(path="secrets.Google_Drive.folder_ids").root)
-        mark_as_initialized()
+    if not initializer.check_if_initialized():
+        initializer.initialize_folders(country_markets, handler.root_folder_id)
+        initializer.mark_as_initialized()
 
     # Proceed with other operations (e.g., upload, download, etc.)
     print("Google Drive setup complete. Proceeding with other tasks...")
